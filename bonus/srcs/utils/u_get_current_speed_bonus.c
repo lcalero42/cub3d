0df675663ec8f7d58185d45396f7626e8019621a
Enzo Keisler <enzokeisler89@gmail.com>
2025-08-26 17:03:14 +0200

    Chore: removed unused printf

--- bonus/srcs/utils/u_get_current_speed_bonus.c
+++ bonus/srcs/utils/u_get_current_speed_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ekeisler <ekeisler@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 13:33:10 by ekeisler          #+#    #+#             */
-/*   Updated: 2025/08/26 16:47:13 by ekeisler         ###   ########.fr       */
+/*   Updated: 2025/08/26 17:02:36 by ekeisler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,7 @@
 double u_get_current_speed(t_data *data)
 {
 	if (data->player.is_running)
-	{
-		printf("run\n");
 		return (MOVE_SPEED * RUN_MULTIPLIER);
-	}
 	else
 		return (MOVE_SPEED);
 }
