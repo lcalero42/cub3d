a2eec262cd89fcc49b10da3ae637a949484c41c5
ekeisler <enzokeisler89@gmail.com>
2025-09-30 20:00:46 +0200

    Fix: player position on the minimap

--- bonus/srcs/rendering/re_minimap_bonus.c
+++ bonus/srcs/rendering/re_minimap_bonus.c
@@ -6,72 +6,55 @@
 /*   By: ekeisler <ekeisler@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 10:45:42 by ekeisler          #+#    #+#             */
-/*   Updated: 2025/09/25 15:15:07 by ekeisler         ###   ########.fr       */
+/*   Updated: 2025/09/30 19:48:14 by ekeisler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-static void draw_minimap_background(t_data *data, int start_x, int start_y, int size);
 static void draw_square(t_data *data, int x, int y, int color, int scale);
+static void draw_minimap_player_with_direction(t_data *data, int start_x, int start_y, int cell_size);
+static void draw_line(t_data *data, int x0, int y0, int x1, int y1, int color);
 
 void	render_minimap(t_data *data)
 {
-    int	minimap_size;
-    int	minimap_margin;
-    int	cell_size;
-    int	start_x;
-    int	start_y;
-    int	i;
-    int	j;
-    int	pixel_x;
-    int	pixel_y;
+	int	minimap_size;
+	int	minimap_margin;
+	int	cell_size;
+	int	start_x;
+	int	start_y;
+	int	i;
+	int	j;
+	int	pixel_x;
+	int	pixel_y;
 
-    cell_size = WINDOW_WIDTH / 80;
-    if (cell_size < 8)
-        cell_size = 8;
-    if (cell_size > 20)
-        cell_size = 20;
-    minimap_margin = WINDOW_WIDTH / 100;
-    minimap_size = fmax(data->grid.width, data->grid.height) * cell_size;
-    start_x = minimap_margin;
-    start_y = minimap_margin;
-    draw_minimap_background(data, start_x, start_y, minimap_size);
-    i = 0;
-    while (data->grid.grid[i])
-    {
-        j = 0;
-        while (data->grid.grid[i][j])
-        {
-            pixel_x = start_x + j * cell_size;
-            pixel_y = start_y + i * cell_size;
-            if (data->grid.grid[i][j] == '1')
-                draw_square(data, pixel_x, pixel_y, 0x000000, cell_size);
-            else if (data->grid.grid[i][j] == '0')
-                draw_square(data, pixel_x, pixel_y, 0xFFFFFF, cell_size);
-            else if (data->grid.grid[i][j] == 'S')
-                draw_square(data, pixel_x, pixel_y, 0x0F0F00, cell_size);
-            j++;
-        }
-        i++;
-    }
-}
-
-static void	draw_minimap_background(t_data *data, int start_x, int start_y, int size)
-{
-	int	x;
-	int	y;
-
-	y = 0;
-	while (y < size)
+	cell_size = WINDOW_WIDTH / 120;
+	if (cell_size < 5)
+		cell_size = 5;
+	if (cell_size > 16)
+		cell_size = 16;
+	minimap_margin = WINDOW_WIDTH * 0.05;
+	minimap_size = fmax(data->grid.width, data->grid.height) * cell_size;
+	start_x = WINDOW_WIDTH - minimap_size;
+	start_y = minimap_margin;
+	i = 0;
+	while (data->grid.grid[i])
 	{
-		x = 0;
-		while (x < size)
+		j = 0;
+		while (data->grid.grid[i][j])
 		{
-			put_pixel_to_image(data, start_x + x, start_y + y, 0x404040);
-			x++;
+			pixel_x = start_x + j * cell_size;
+			pixel_y = start_y + i * cell_size;
+			if (data->grid.grid[i][j] != ' ')
+				draw_square(data, pixel_x, pixel_y, 0xFFFFFF, cell_size);
+			if (data->grid.grid[i][j] == '1')
+				draw_square(data, pixel_x, pixel_y, 0x000000, cell_size);
+			if (data->grid.grid[i][j] == '2')
+				draw_square(data, pixel_x, pixel_y, 0x8B4513, cell_size);
+			draw_minimap_player_with_direction(data, start_x, start_y, cell_size);
+			j++;
 		}
-		y++;
+		i++;
 	}
 }
 
@@ -91,4 +74,57 @@ static void	draw_square(t_data *data, int x, int y, int color, int scale)
 		}
 		i++;
 	}
+}
+
+static void draw_line(t_data *data, int x0, int y0, int x1, int y1, int color)
+{
+	int	dx;
+	int	dy;
+	int	sx;
+	int	sy;
+	int	err;
+	int	e2;
+	int	x;
+	int	y;
+	
+	dx = abs(x1 - x0);
+	dy = abs(y1 - y0);
+	sx = x0 < x1 ? 1 : -1;
+	sy = y0 < y1 ? 1 : -1;
+	err = dx - dy;
+	x = x0;
+	y = y0;
+	while (1)
+	{
+		put_pixel_to_image(data, x, y, color);
+		if (x == x1 && y == y1)
+			break;
+		e2 = 2 * err;
+		if (e2 > -dy)
+		{
+			err -= dy;
+			x += sx;
+		}
+		if (e2 < dx)
+		{
+			err += dx;
+			y += sy;
+		}
+	}
+}
+
+static void draw_minimap_player_with_direction(t_data *data, int start_x, int start_y, int cell_size)
+{
+	int player_pixel_x;
+	int	player_pixel_y;
+	int dir_end_x;
+	int	dir_end_y;
+
+	player_pixel_x = start_x + (int)(data->player.position.x * cell_size);
+	player_pixel_y = start_y + (int)(data->player.position.y * cell_size);
+	draw_square(data, player_pixel_x - cell_size/4, player_pixel_y - cell_size/4, 
+			   0xFF0000, cell_size/2);
+	dir_end_x = player_pixel_x + (int)(data->player.dir.x * cell_size);
+	dir_end_y = player_pixel_y + (int)(data->player.dir.y * cell_size);
+	draw_line(data, player_pixel_x, player_pixel_y, dir_end_x, dir_end_y, 0xFF0000);
 }
